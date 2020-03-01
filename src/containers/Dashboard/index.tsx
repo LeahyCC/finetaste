6195af3366f7225ec58438e9c5781db8cdaa326b
Colin Leahy <leahycc@pm.me>
2020-03-01 00:15:37 -0600

    navigation setup

--- src/containers/Dashboard/index.tsx
+++ src/containers/Dashboard/index.tsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import { GiPineapple } from 'react-icons/gi'
 
 // eslint-disable-next-line react/prefer-stateless-function
 export class DashBoard extends Component {
   render() {
-    return <div>DashBoard</div>
+    return (
+      <div>
+        DashBoard <GiPineapple size={20} />
+      </div>
+    )
   }
 }
 
