80c5ca12fd7e6d8ad921a5e298c65d8ede2c7aa8
LeahyCC <Colin Leahy>
2020-03-14 15:09:30 -0500

    dash favorite recipes

--- src/redux/actions/MOCKDATA.ts
+++ src/redux/actions/MOCKDATA.ts
@@ -3,8 +3,9 @@ import { recipesObj } from '../../objects/recipes'
 const recipeMockData: recipesObj[] = [
   {
     id: '1',
-    name: 'pizza',
+    name: 'favorite pizza',
     cuisine: 'dinner',
+    favorite: true,
     ingredients: [
       {
         itemCount: 1,
@@ -40,6 +41,39 @@ const recipeMockData: recipesObj[] = [
     id: '2',
     name: 'potato whip it good',
     cuisine: 'breakfast',
+    favorite: false,
+    ingredients: [
+      {
+        itemCount: 1,
+        itemName: 'potato'
+      },
+      {
+        itemCount: 2,
+        itemName: 'garlic cloves'
+      }
+    ],
+    mealTime: {
+      prep: '10m',
+      cook: '15m',
+      ready: '20m'
+    },
+    directions: [
+      {
+        step: 1,
+        direction: 'whip it good, real good'
+      },
+      {
+        step: 2,
+        direction: 'whip it even gooder'
+      }
+    ],
+    notes: 'Note'
+  },
+  {
+    id: '3',
+    name: 'favorite potato whip it good 3',
+    cuisine: 'breakfast',
+    favorite: true,
     ingredients: [
       {
         itemCount: 1,
